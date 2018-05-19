6feaadfb9d48221e7d1ef01c2accbf4bf56faae2
password <mnurilmanbaehaqi@gmail.com>
2018-05-19 16:31:36 +0700

    add maenu, footer, and set menu clickable

--- app/src/main/java/com/moxspoy/mnurilmanbaehaqi/holodist/LauncherActivity.java
+++ app/src/main/java/com/moxspoy/mnurilmanbaehaqi/holodist/LauncherActivity.java
@@ -1,13 +1,28 @@
 package com.moxspoy.mnurilmanbaehaqi.holodist;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class LauncherActivity extends AppCompatActivity {
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        btnNext = findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
